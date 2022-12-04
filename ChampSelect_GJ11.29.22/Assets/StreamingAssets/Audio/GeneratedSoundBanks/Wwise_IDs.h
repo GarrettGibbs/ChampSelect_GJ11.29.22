81206cb761ca23bbaf3a0d282b1e8af3983d789d
Williamatthewood <w.wood@tcu.edu>
2022-12-04 10:21:34 -0600

    Test of Wwise integration
    Project removed, left generated soundbanks

---
+++ ChampSelect_GJ11.29.22/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DESTROY_OBJECT = 2524325405U;
+        static const AkUniqueID FIRE_LASER_ENEMY = 167102348U;
+        static const AkUniqueID HEAL_PLAYER = 1445354741U;
+        static const AkUniqueID SHIP_EXPLOSION_ENEMY = 386900386U;
+        static const AkUniqueID SPACESHIP_ENGINE_ENEMY = 2180772097U;
+        static const AkUniqueID SPACESHIP_ENGINE_PLAYER = 2689741048U;
+        static const AkUniqueID TAKE_DAMAGE_PLAYER = 3762463012U;
+        static const AkUniqueID TRACTORBEAM_GRAB = 2503555200U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID IGN_ENG_EXH_POSITION = 1776289502U;
+        static const AkUniqueID IGN_GEARBOX_THROTTLE = 3744760799U;
+        static const AkUniqueID IGN_HARLEY_GRAIN_LOOP_BLEND = 1573260114U;
+        static const AkUniqueID IGN_LOAD = 1046277832U;
+        static const AkUniqueID IGN_RPM = 2202868459U;
+        static const AkUniqueID IGNITERSYNTH_REV = 1355046805U;
+        static const AkUniqueID IGNITERSYNTH_STATE = 3696491809U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUND_FX = 1914831615U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
